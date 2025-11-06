52c40423f6fbf06f4ca692290e4314d2427e1f16
Victor Marin Escribano <victor.marin4@educa.madrid.org>
2025-11-06 19:45:24 +0100

    inicio proyecto, creacion de entorno de trabajo

---
+++ src/main/java/dev/luisvives/trabajoprogramacionsegundo/TrabajoProgramacionSegundoApplication.java
@@ -0,0 +1,13 @@
+package dev.luisvives.trabajoprogramacionsegundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoProgramacionSegundoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrabajoProgramacionSegundoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/luisvives/trabajoprogramacionsegundo/TrabajoProgramacionSegundoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.luisvives.trabajoprogramacionsegundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoProgramacionSegundoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
